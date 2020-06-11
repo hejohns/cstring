87925fa92b1b2c42d4f99f02f5d19cf7f4f40e12
hejohns <hejohns@umich.edu>
2020-06-10 22:44:39 -0400

    add basic complexity info to comments

--- bheap/bheap.h
+++ bheap/bheap.h
@@ -55,6 +55,7 @@ static void bheap_ ## T ## _free(bheap_ ## T *pq){         \
 }                                                          \
                                                            \
 static void bheap_ ## T ## _siftDown(bheap_ ## T *pq, size_t index){\
+    /* O(log(height of tree - height of index)) */         \
     size_t index_lchild; /* left child or lesser child */  \
     while(2*index+1 <= pq->size-1){                        \
         index_lchild = 2*index+1;                          \
@@ -76,6 +77,7 @@ static void bheap_ ## T ## _siftDown(bheap_ ## T *pq, size_t index){\
 }                                                          \
                                                            \
 static void bheap_ ## T ## _siftUp(bheap_ ## T *pq, size_t index){\
+    /* O(log(height of index)) */                          \
     while(index != 0){                                     \
         if(pq->less(pq->arr+((index-1)/2), pq->arr+index)){\
             {                                              \
@@ -93,19 +95,21 @@ static void bheap_ ## T ## _siftUp(bheap_ ## T *pq, size_t index){\
                                                            \
 static void bheap_ ## T ## _make_heap(bheap_ ## T *pq){    \
     /* Floyd's heap algorithm */                           \
+    /* O(size) */                                          \
     if(pq->size <= 1){                                     \
         return;                                            \
     }                                                      \
     size_t height = 0;                                     \
     for(size_t sz = pq->size >> 1; sz; height++, sz >>= 1){\
     }                                                      \
-    for(size_t i=(1<<height)-2; i > 0; i--){              \
+    for(size_t i=(1<<height)-2; i > 0; i--){               \
         bheap_ ## T ## _siftDown(pq, i);                   \
     }                                                      \
     bheap_ ## T ## _siftDown(pq, 0);                       \
 }                                                          \
                                                            \
 static void bheap_ ## T ## _push(bheap_ ## T *pq, const T value){\
+    /* O(log size) */                                      \
     if(!(pq->size < pq->capacity)){                        \
         T* tmp = realloc(pq->arr, 2*(pq->size*sizeof(T))); \
         if(tmp){                                           \
@@ -116,8 +120,7 @@ static void bheap_ ## T ## _push(bheap_ ## T *pq, const T value){\
             exit(EXIT_FAILURE);                            \
         }                                                  \
     }                                                      \
-    (pq->arr)[pq->size] = value;                           \
-    pq->size++;                                            \
+    (pq->arr)[pq->size++] = value;                         \
     bheap_ ## T ## _siftUp(pq, pq->size-1);                \
 }                                                          \
                                                            \
@@ -126,6 +129,7 @@ static void bheap_ ## T ## _push_by_ref(bheap_ ## T *pq, const T *value){\
 }                                                          \
                                                            \
 static void bheap_ ## T ## _pop(bheap_ ## T *pq){          \
+    /* O(log size) */                                      \
     if(pq->size > 0){                                      \
         (pq->arr)[0] = (pq->arr)[pq->size-1];              \
         pq->size--;                                        \
