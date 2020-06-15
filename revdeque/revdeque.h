f76bab2815815807e30af39c03f46d78fba6bd65
hejohns <hejohns@umich.edu>
2020-06-15 03:38:41 -0400

    def super bugged out but done?

--- revdeque/revdeque.h
+++ revdeque/revdeque.h
@@ -7,6 +7,7 @@
 #include <deque>
 #include <cmath>
 #include <algorithm>
+#include <cassert>
 
 template <typename T, typename index_type = size_t>
 class revdeque{
@@ -50,8 +51,23 @@ class revdeque{
         }
     }
     void revert(index_type start, index_type end){ //[start, end)
+        assert(end != 0);
         coord start_c = loc(start);
         coord end_c = loc(end);
+        if(end_c.pos == 0){
+            end_c.bin--;
+            end_c.pos = top[end_c.bin].contents.size;
+        }
+        if(start_c.bin < end_c.bin){
+            revert_helper(top[start_c.bin], start_c.pos, top[start_c.bin].contents.size());
+        }
+        for(index_type i=start_c.bin+1; i < end_c.bin; i++){
+            revert_helper(top[i], 0, top[i].contents.size());
+        }
+        if(true){
+            revert_helper(top[end_c.bin], 0, end_c.pos);
+        }
+        std::reverse(top.begin()+start_c.bin, top.begin()+end_c.bin);
     }
     private:
     coord loc(index_type pos){
@@ -71,9 +87,33 @@ class revdeque{
             bin.reversed = !bin.reversed;
         }
         else{
-            if(start_c.pos < bin.contents.size()-end_c.pos){
+            if(start_pos < bin.contents.size()-end_pos){
+                std::vector<T> tmp((std::vector<T>::size_type)start_pos);
+                std::copy(bin.contents.begin(), bin.contents.begin()+start_pos, tmp.begin());
+                bin.contents.erase(bin.contents.begin(), bin.contents.begin()+start_pos);
+                for(index_type i=0; i < bin.contents.size()-end_pos; i++){
+                    bin.contents.emplace_front(bin.contents.back());
+                    bin.contents.pop_back();
+                }
+                for(index_type i=0; i < start_pos; i++){
+                    bin.contents.emplace_back(tmp.back());
+                    tmp.pop_back();
+                }
+                bin.reversed = !bin.reversed;
             }
             else{
+                std::vector<T> tmp((std::vector<T>::size_type)bin.contents.size()-end_pos);
+                std::copy(bin.contents.begin()+(end_pos-1), bin.contents.end(), tmp.begin());
+                bin.contents.erase(bin.contents.begin()+(end_pos-1), bin.contents.end());
+                for(index_type i=0; i < start_pos; i++){
+                    bin.contents.emplace_back(bin.contents.front());
+                    bin.contents.pop_front();
+                }
+                for(index_type i=0; i < bin.contents.size()-end_pos; i++){
+                    bin.contents.emplace_front(tmp.back());
+                    tmp.pop_back();
+                }
+                bin.reversed = !bin.reversed;
             }
         }
     }
