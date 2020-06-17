3b6000ec9d546946aaedebf1192d7b3c79acf9a2
hejohns <hejohns@umich.edu>
2020-06-16 23:19:02 -0400

    man all these reversals are confusing

--- revdeque/revdeque_benchmark.cpp
+++ revdeque/revdeque_benchmark.cpp
@@ -6,33 +6,21 @@
 #include "revdeque.h"
 
 int main(){
-    const size_t sz = 10*10;
+    const size_t sz = 50*50;
     revdeque<unsigned int, unsigned int> revdeque_inst(sz);
-    std::vector<unsigned int> reference(sz);
     for(size_t i=0; i<sz; i++){
         revdeque_inst[i] = i;
-        reference[i] = i;
     }
     for(unsigned int h=0; h < 1<<1; h++){
         for(unsigned int i=sz-3; i >= 2; i--){
             for(unsigned int j=1; j+i < sz; j++){
                 revdeque_inst.revert(j, j+i);
-                std::reverse(reference.begin()+j, reference.begin()+j+i);
-                for(unsigned int k=0; k<sz; k++){
-                    if(revdeque_inst[k] != reference[k]){
-                        std::cerr << "Failed\n";
-                        assert(0);
-                    }
-                }
             }
         }
     }
-    std::cout << "Ok\n";
-    /*
     for(unsigned int i=0; i<sz; i++){
         std::cout<<revdeque_inst[i]<<' ';
     }
-    */
     return 0;
 }
 
--- revdeque/revdeque_test.cpp
+++ revdeque/revdeque_test.cpp
@@ -6,109 +6,25 @@
 #include "revdeque.h"
 
 int main(){
-    const size_t sz = 10000;
+    const size_t sz = 10*10;
     revdeque<unsigned int, unsigned int> revdeque_inst(sz);
     std::vector<unsigned int> reference(sz);
     for(size_t i=0; i<sz; i++){
         revdeque_inst[i] = i;
         reference[i] = i;
     }
-    /*
-    revdeque_inst.revert(1, 190);
-    std::reverse(reference.begin()+1, reference.begin()+190);
-    for(size_t i=0; i<sz; i++){
-        if(revdeque_inst[i] != reference[i]){
-            std::cout << "Failed\n";
-            assert(0);
-            exit(EXIT_FAILURE);
-        }
-    }
-    */
-    revdeque_inst.revert(1, 290);
-    std::reverse(reference.begin()+1, reference.begin()+290);
-    for(size_t i=0; i<sz; i++){
-        if(revdeque_inst[i] != reference[i]){
-            std::cout << "Failed\n";
-            assert(0);
-            exit(EXIT_FAILURE);
-        }
-    }
-    revdeque_inst.revert(100, 200);
-    std::reverse(reference.begin()+100, reference.begin()+200);
-    for(size_t i=0; i<sz; i++){
-        if(revdeque_inst[i] != reference[i]){
-            std::cout << "Failed\n";
-            assert(0);
-            exit(EXIT_FAILURE);
-        }
-    }
-    revdeque_inst.revert(1, 152);
-    std::reverse(reference.begin()+1, reference.begin()+152);
-    for(size_t i=0; i<sz; i++){
-        if(revdeque_inst[i] != reference[i]){
-            std::cout << "Failed\n";
-            assert(0);
-            exit(EXIT_FAILURE);
-        }
-    }
-    revdeque_inst.revert(49, 151);
-    std::reverse(reference.begin()+49, reference.begin()+151);
-    for(size_t i=0; i<sz; i++){
-        if(revdeque_inst[i] != reference[i]){
-            std::cout << "Failed\n";
-            assert(0);
-            exit(EXIT_FAILURE);
-        }
-    }
-    /*
-    */
-    /*
-    revdeque_inst.revert(70, 110);
-    std::reverse(reference.begin()+70, reference.begin()+110);
-    revdeque_inst.revert(90, 110);
-    std::reverse(reference.begin()+90, reference.begin()+110);
-    revdeque_inst.revert(10, 90);
-    std::reverse(reference.begin()+10, reference.begin()+90);
-    revdeque_inst.revert(20, 80);
-    std::reverse(reference.begin()+20, reference.begin()+80);
-    revdeque_inst.revert(10, 580);
-    std::reverse(reference.begin()+10, reference.begin()+580);
-    revdeque_inst.revert(410, 580);
-    std::reverse(reference.begin()+410, reference.begin()+580);
-    revdeque_inst.revert(0, 100);
-    std::reverse(reference.begin(), reference.begin()+100);
-    revdeque_inst.revert(50, 150);
-    std::reverse(reference.begin()+50, reference.begin()+150);
-    revdeque_inst.revert(70, 110);
-    std::reverse(reference.begin()+70, reference.begin()+110);
-    revdeque_inst.revert(540, 545);
-    std::reverse(reference.begin()+540, reference.begin()+545);
-    revdeque_inst.revert(540, 545);
-    std::reverse(reference.begin()+540, reference.begin()+545);
-    revdeque_inst.revert(40, 545);
-    std::reverse(reference.begin()+40, reference.begin()+545);
-    revdeque_inst.revert(40, 595);
-    std::reverse(reference.begin()+40, reference.begin()+595);
-    revdeque_inst.revert(2, 99);
-    std::reverse(reference.begin()+2, reference.begin()+99);
-    revdeque_inst.revert(2, 101);
-    std::reverse(reference.begin()+2, reference.begin()+101);
-    revdeque_inst.revert(237, 737);
-    std::reverse(reference.begin()+237, reference.begin()+737);
-    revdeque_inst.revert(0, 999);
-    std::reverse(reference.begin()+0, reference.begin()+999);
-    revdeque_inst.revert(100, 199);
-    std::reverse(reference.begin()+100, reference.begin()+199);
-    revdeque_inst.revert(1, 9);
-    std::reverse(reference.begin()+1, reference.begin()+9);
-    revdeque_inst.revert(1, 100);
-    std::reverse(reference.begin()+1, reference.begin()+100);
-    */
-    for(size_t i=0; i<sz; i++){
-        if(revdeque_inst[i] != reference[i]){
-            std::cout << "Failed\n";
-            assert(0);
-            exit(EXIT_FAILURE);
+    for(unsigned int h=0; h < 1<<1; h++){
+        for(unsigned int i=sz-3; i >= 2; i--){
+            for(unsigned int j=1; j+i < sz; j++){
+                revdeque_inst.revert(j, j+i);
+                std::reverse(reference.begin()+j, reference.begin()+j+i);
+                for(unsigned int k=0; k<sz; k++){
+                    if(revdeque_inst[k] != reference[k]){
+                        std::cerr << "Failed\n";
+                        assert(0);
+                    }
+                }
+            }
         }
     }
     std::cout << "Ok\n";
