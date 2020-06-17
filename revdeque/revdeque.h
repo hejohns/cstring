e5049035c9153711a307869dc6d577910deb7540
hejohns <hejohns@umich.edu>
2020-06-17 04:38:46 -0400

    missed one

--- revdeque/revdeque.h
+++ revdeque/revdeque.h
@@ -114,7 +114,7 @@ class revdeque{
         auto bin_it = std::lower_bound(bin_list.begin(), bin_list.end(), pos,\
                 [](const bin& left, index_type value)->bool{return left.tail < value;});
         pos -= bin_it->head;
-        index_type bin = (index_type)bin_it-bin_list.begin();
+        index_type bin = (index_type)(bin_it-bin_list.begin());
         return coord{bin, pos};
     }
     void revert_within_single_bin(bin& bin, index_type start_pos, index_type end_pos){
